08441c01203b59513a72a8ef949ca881164af4e2
ivrouwe <ivan.donald.vrouwe@gmail.com>
2018-02-09 23:01:19 -0700

    Create general-purpose function for replacing placeholder content with JSON + Handlebars content. Use said function to populate the Photo Gallery section in the index file with JSON content.

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,20 @@
 (function($) {
 
+	function populateElement(selector, rawTemplate, rawData) {
+		var element = $(selector),
+			placeholderChildren = element.children().not('#' + element.attr('aria-labelledby')),
+			template = Handlebars.compile(rawTemplate);
+			content = $.parseHTML(template(rawData));
+
+		placeholderChildren.remove();
+		element.append(content);
+	}
+
+	$.get('assets/data/photos.json', function(data) {
+		var photoData = data;
+
+		$.get('assets/templates/photo-gallery-list.handlebars', function(data) {
+			populateElement('#photo-gallery', data, photoData);
+		});
+	});
 })(jQuery);
