911ddf72cdeba0b8b225c8f5e178b31798924e79
ivrouwe <ivan.donald.vrouwe@gmail.com>
2018-02-12 15:51:26 -0700

    Get maps data. Replace placeholder content with a list of maps.

--- assets/js/global.js
+++ assets/js/global.js
@@ -13,5 +13,30 @@
 		});
 	});
 
+	$.get('assets/data/maps.json', function(data) {
+		var element = $('#maps'),
+			placeholderContent = element.children().not('#' + element.attr('aria-labelledby') + ', #' + element.attr('aria-describedby')),
+			maps,
+			defaultZoom = 10;
+
+		maps = data.maps;
+		placeholderContent.replaceWith('<ul></ul>');
+
+		maps.forEach(function(map) {
+			var output;
+
+			if (!map.zoom) {
+				map.zoom = defaultZoom;
+			}
+
+			element.children('ul').append('<li data-contains="A map of ' + map.name + '"><dl class="map" data-name="A map of ' + map.name + '"><dt class="map-name">A map of ' + map.name + '</dt><dd class="map-content"></dd></dl></li>');
+			
+			output = new google.maps.Map(element.find('[data-name="A map of ' + map.name + '"] .map-content')[0], {
+				zoom: map.zoom,
+				center: map.center
+			});
+		});
+	});
+
 	$('html').attr('data-js', 'true');
 })(jQuery);
