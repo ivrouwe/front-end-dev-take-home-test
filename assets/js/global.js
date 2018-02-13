ba4792dbebd9e63e832e2da3fcb9de82d951a61b
ivrouwe <ivan.donald.vrouwe@gmail.com>
2018-02-12 21:17:46 -0700

    Add markers and basic infowindow output and functionality to new maps.

--- assets/js/global.js
+++ assets/js/global.js
@@ -60,7 +60,7 @@
 	$.get('assets/data/maps-preformatted.json', function(data) {
 		var element = $('#maps'),
 			maps = data.resto,
-			defaultZoom = 11;
+			defaultZoom = 13;
 
 		if(!element.children('.maps-list').length) {
 			populateElement('#maps', '<ul class="maps-list"></ul>');
@@ -91,7 +91,7 @@
 				map.location.zoom = defaultZoom;
 			}
 
-			element.children('ul').append('<li data-contains="' + name + '"><dl class="map" data-name="' + name + '"><dt class="map-name">' + name + '</dt><dd class="map-content"></dd></dl></li>');
+			element.children('ul').append('<li data-contains="' + name + '"><dl class="map" data-name="' + name + '" aria-live="polite"><dt class="map-name">' + name + '</dt><dd class="map-content"></dd></dl></li>');
 			
 			output = new google.maps.Map(element.find('[data-name="' + name + '"] .map-content')[0], {
 				zoom: map.location.zoom,
@@ -100,6 +100,90 @@
 					lng: parseFloat(map.location.longitude)
 				}
 			});
+
+			if(map.nearby_restaurants) {
+				map.nearby_restaurants.forEach(function(object) {
+					var restaurant = object.restaurant,
+						marker,
+						infoWindow,
+						infoWindowContent;
+
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(parseFloat(restaurant.location.latitude), parseFloat(restaurant.location.longitude)),
+						icon: 'https://maps.google.com/mapfiles/kml/pal2/icon40.png',
+						map: output
+					});
+
+					marker.addListener('click', function() {
+						var restaurantData,
+							cuisines,
+							index;
+
+						if(restaurant.name && restaurant.id && restaurant.name !== '' && restaurant.id !== null) {
+							infoWindowContent = $(document.createElement('article'));
+							infoWindowContent.addClass('info-window');
+							infoWindowContent.addClass('restaurant');
+							infoWindowContent.attr('id', 'restaurant-' + restaurant.id.trim());
+							infoWindowContent.attr('aria-labelledby', 'restaurant-' + restaurant.id.trim() + '-heading');
+							infoWindowContent.append('<header><h1 id="' + 'restaurant-' + restaurant.id.trim() + '-heading">' + restaurant.name + '</h1></header>');
+
+							if(restaurant.thumb && restaurant.thumb !== '') {
+								if(restaurant.featured_image && restaurant.featured_image !== '') {
+									infoWindowContent.children('header').append('<a href="' + restaurant.featured_image.trim() + '"><img src="' + restaurant.thumb.trim() + '" alt="View Featured Image"></a>');
+								} else {
+									infoWindowContent.children('header').append('<div class="restaurant-thumbnail"><img src="' + restaurant.thumb.trim() + '" alt="" role="presentation"</div>');
+								}
+							}
+
+							infoWindowContent.append('<dl class="restaurant-data"></dl>');
+							
+							restaurantData = infoWindowContent.children('.restaurant-data');
+
+							if (restaurant.location.address && restaurant.location.address !== '') {
+								restaurantData.append('<dt>Address</dt>');
+								restaurantData.append('<dd>' + restaurant.location.address.trim() + '</dd>');
+							}
+
+							if(restaurant.cuisines && restaurant.cuisines !== '') {
+								cuisines = restaurant.cuisines.split(',');
+
+								restaurantData.append('<dt>Cuisines</dt>');
+
+								for(index = 0; index < cuisines.length; index++) {
+									restaurantData.append('<dd>' + cuisines[index].trim() + '</dd>');
+								}
+							}
+
+							if(restaurant.average_cost_for_two && restaurant.currency && restaurant.average_cost_for_two !== null && restaurant.currency !== '') {
+								restaurantData.append('<dt>Average Cost (For Two)</dt>');
+								restaurantData.append('<dd>' + restaurant.currency.trim() + parseFloat(restaurant.average_cost_for_two) + '</dd>');
+							}
+
+							if(restaurant.price_range && restaurant.price_range !== null) {
+								restaurantData.append('<dt>Price Range</dt>');
+								restaurantData.append('<dd>' + parseInt(restaurant.price_range) + '</dd>');
+							}
+
+
+							if(restaurant.offers && restaurant.offers.length) {
+								restaurantData.append('<dt>Offers</dt>');
+
+								for(index = 0; index < restaurant.offers.length; index++) {
+									restaurantData.append('<dd>' + restaurant.offers[index].trim() + '</dd>');
+								}
+							}
+
+							console.log(infoWindowContent[0]);
+
+							infoWindow = new google.maps.InfoWindow({
+								content: infoWindowContent[0],
+							});
+
+							infoWindow.open(output, marker);
+						}
+					});
+				});
+			}
 		});
 	});
 
